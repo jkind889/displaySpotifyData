8ec09eb48fb1041e5f52eca6e559c86720dab086
jkind889 <jova.kind@bison.howard.edu>
2025-12-25 10:06:05 -1000

    Addition - EventListener added for redirect uri
    Added the eventlistener for my redirect uri so the authcodeflow
    isnt called as soon as the page is loaded, as well as added
    fcn.js to hold my main functions

---
+++ Display/src/fcns.js
@@ -0,0 +1,27 @@
+export async function fetchProfile(token) {
+    const result = await fetch("https://api.spotify.com/v1/me", {
+        method: "GET", headers: { Authorization: `Bearer ${token}` }
+    });
+
+    return await result.json();
+}
+
+export async function fetchArtists(token)
+{
+    const result = await fetch("https://api.spotify.com/v1/me/top/artists", {
+        method: "GET", headers: { Authorization: `Bearer ${token}`}
+    });
+
+    return await result.json();
+    
+}
+
+export async function fetchAlbums(token)
+{
+    const result = await fetch("https://api.spotify.com/v1/me/top/tracks?time_range=long_term&limit=40&offset=0", {
+        method: "GET", headers: { Authorization: `Bearer ${token}`}
+    });
+
+    return await result.json();
+
+}
